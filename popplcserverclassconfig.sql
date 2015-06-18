d798e508ce29caf78acbb91ca1056e5e70dc9094
gowrishdba <gowrishdba@yahoo.com>
2015-06-18 12:59:28 +0530

    Create popplcserverclassconfig.sql

---
+++ popplcserverclassconfig.sql
@@ -0,0 +1,8 @@
+
+insert into PLC_SERVER_CLASS_CONFIG values (1, 'Dell', 'PowerEdge R710', 0,0,null, null, null, null, null);
+insert into PLC_SERVER_CLASS_CONFIG values (2, 'Dell', 'PowerEdge R910', 0,0,null, null, null, null, null);
+insert into PLC_SERVER_CLASS_CONFIG values (3, 'Dell', 'PowerEdge R620', 0,0,null, null, null, null, null);
+insert into PLC_SERVER_CLASS_CONFIG values (4, 'Dell', 'PowerEdge R900', 0,0,null, null, null, null, null);
+insert into PLC_SERVER_CLASS_CONFIG values (5, 'Dell', 'x86_64', 0,0,null, null, null, null, null); -- this is actually M620
+
+commit;
