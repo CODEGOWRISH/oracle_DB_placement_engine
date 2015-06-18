5ca8685d0024facd156f1482adb14113c4085332
gowrishdba <gowrishdba@yahoo.com>
2015-06-18 12:56:17 +0530

    Create popplcclusterlimits.sql

---
+++ popplcclusterlimits.sql
@@ -0,0 +1,13 @@
+insert into PLC_CLUSTER_LIMITS values (1, 'Non Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (2, 'Non Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (3, 'Non Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (4, 'Non Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (5, 'Non Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+
+insert into PLC_CLUSTER_LIMITS values (1, 'Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (2, 'Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (3, 'Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (4, 'Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+insert into PLC_CLUSTER_LIMITS values (5, 'Prod', 'RAC', 2, 30, 100, 10, 30, 0, 0, 65, 65, 65, 65, 65, 65);
+
+commit;
