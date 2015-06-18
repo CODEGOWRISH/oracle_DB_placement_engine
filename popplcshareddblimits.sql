9a930016c316c184f73930d4bd9d7bd34d959fc3
gowrishdba <gowrishdba@yahoo.com>
2015-06-18 13:00:01 +0530

    Create popplcshareddblimits.sql

---
+++ popplcshareddblimits.sql
@@ -0,0 +1,18 @@
+
+insert into PLC_SHARED_DB_LIMITS values (1, 'Prod', 'RAC', 16, 24, 4, 6, 6);
+insert into PLC_SHARED_DB_LIMITS values (1, 'Non Prod', 'RAC', 16, 24, 4, 6, 8);
+
+insert into PLC_SHARED_DB_LIMITS values (2, 'Prod', 'RAC', 16, 24, 4, 6, 6);
+insert into PLC_SHARED_DB_LIMITS values (2, 'Non Prod', 'RAC', 16, 24, 4, 6, 8);
+
+insert into PLC_SHARED_DB_LIMITS values (3, 'Prod', 'RAC', 16, 24, 4, 6, 6);
+insert into PLC_SHARED_DB_LIMITS values (3, 'Non Prod', 'RAC', 16, 24, 4, 6, 8);
+
+insert into PLC_SHARED_DB_LIMITS values (4, 'Prod', 'RAC', 16, 24, 4, 6, 6);
+insert into PLC_SHARED_DB_LIMITS values (4, 'Non Prod', 'RAC', 16, 24, 4, 6, 8);
+
+insert into PLC_SHARED_DB_LIMITS values (5, 'Prod', 'RAC', 16, 24, 4, 6, 6);
+insert into PLC_SHARED_DB_LIMITS values (5, 'Non Prod', 'RAC', 16, 24, 4, 6, 8);
+
+
+commit;
