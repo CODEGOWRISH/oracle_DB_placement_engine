a13ec083e5b92db2c131964d739fe1c3839cc4d7
gowrishdba <gowrishdba@yahoo.com>
2015-06-18 12:57:12 +0530

    Create popplcclustersexcluded.sql

---
+++ popplcclustersexcluded.sql
@@ -0,0 +1,7 @@
+
+alter session set current_schema=ORAODRMGR;
+
+insert into PLC_CLUSTERS_EXCLUDED values ('RCLP35', 'Dedicated to specific project');
+insert into PLC_CLUSTERS_EXCLUDED values ('RCLD04', 'Cluster overloaded');
+
+commit
