2e0d12fe6ace31473712e240125037e6d6ac096d
gowrishdba <gowrishdba@yahoo.com>
2015-07-24 10:58:03 +0530

    Create popplcoscompatdbver.sql

---
+++ popplcoscompatdbver.sql
@@ -0,0 +1,25 @@
+--CREATE TABLE PLC_OS_COMPAT_DB_VER
+--(
+--OS_VERSION                      VARCHAR2(20),
+--COMPATIBLE_DB_VERSION           VARCHAR2(20)
+--);
+
+
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '10.2.0.1');
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '10.2.0.2');
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '10.2.0.3');
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '10.2.0.4');
+
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '11.1.0.7');
+
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '11.2.0.1');
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '11.2.0.2');
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '11.2.0.3');
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL5', '11.2.0.4');
+
+
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL6', '11.2.0.3');
+insert into PLC_OS_COMPAT_DB_VER values ('RHEL6', '11.2.0.4');
+
+
+commit;
