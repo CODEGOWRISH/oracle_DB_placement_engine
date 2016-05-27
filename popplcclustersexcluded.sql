8548888353e3a545ca45ed07abd12a4faede7016
Coder <coderdba@users.noreply.github.com>
2016-05-27 23:20:29 +0530

    Update popplcclustersexcluded.sql

--- popplcclustersexcluded.sql
+++ popplcclustersexcluded.sql
@@ -1,7 +1,7 @@
 
 alter session set current_schema=ORAODRMGR;
 
-insert into PLC_CLUSTERS_EXCLUDED values ('RCLP35', 'Dedicated to specific project');
-insert into PLC_CLUSTERS_EXCLUDED values ('RCLD04', 'Cluster overloaded');
+insert into PLC_CLUSTERS_EXCLUDED values ('RACP35', 'Dedicated to specific project');
+insert into PLC_CLUSTERS_EXCLUDED values ('RACD04', 'Cluster overloaded');
 
 commit
